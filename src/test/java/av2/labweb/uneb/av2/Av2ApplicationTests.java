15b117ec2948e589817413be9cda9c845dfd4432
Vinicius Maciel <vinissmaciel@gmail.com>
2024-12-09 11:14:11 -0300

    first commit

---
+++ src/test/java/av2/labweb/uneb/av2/Av2ApplicationTests.java
@@ -0,0 +1,13 @@
+package av2.labweb.uneb.av2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Av2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
